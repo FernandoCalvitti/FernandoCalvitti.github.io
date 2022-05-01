3f355347449edb5ab7210daa8850a1ca845158bc
FernandoCalvitti <federicofernandogiribaldi@hotmail.com>
2022-05-01 18:35:26 -0300

    Added scrolling effect and fixed side navbar behavior

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -3,12 +3,41 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  function navigateToSection(event, id) {
+    event && event.preventDefault();
+    const elementToView = document.getElementById(id);
+    elementToView.scrollIntoView();
+  }
   return (
     <nav className="app__navbar">
-      <a href="#about_me">About Me</a>
-      <a href="#experience">Experience</a>
-      <a href="#skills">Skills</a>
-      <a href="#contact">Contact</a>
+      <button
+        onClick={(event) => {
+          navigateToSection(event, "about_me");
+        }}
+      >
+        About Me
+      </button>
+      <button
+        onClick={(event) => {
+          navigateToSection(event, "experience");
+        }}
+      >
+        Experience
+      </button>
+      <button
+        onClick={(event) => {
+          navigateToSection(event, "skills");
+        }}
+      >
+        Skills
+      </button>
+      <button
+        onClick={(event) => {
+          navigateToSection(event, "contact");
+        }}
+      >
+        Contact
+      </button>
     </nav>
   );
 };
