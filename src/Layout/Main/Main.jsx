39294fb07e736d7ff2cbefd7e98816fbd8ff6c7d
FernandoCalvitti <83254261+FernandoCalvitti@users.noreply.github.com>
2022-05-02 23:53:51 -0300

    Develop (#4)
    * Added al the folder structure and the components
    
    * Added about me section
    
    * Added my experience section
    
    * Added skills section
    
    * Added contact section
    
    * Added details of contact section
    
    * Added send email functionality
    
    * Added scrolling effect and fixed side navbar behavior
    
    * Added tablet and mobile styles
    
    Co-authored-by: Fernando Calvitti <fcalvitti@indec.gob.ar>

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Welcome from "../../PageSections/Welcome/Welcome";
 import Experience from "../../PageSections/Experience/Experience";
 import Skills from "../../PageSections/Skills/Skills";
@@ -7,6 +7,32 @@ import Contact from "../../PageSections/Contact/Contact";
 import "./Main.css";
 
 const Main = () => {
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+
+    const options = {
+      rootMargin: "0px",
+      threshold: 0.25,
+    };
+
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        const { target } = entry;
+        if (entry.intersectionRatio >= 0.25) {
+          target.classList.add("is-visible");
+        } else {
+          target.classList.remove("is-visible");
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    sections.forEach((section, index) => {
+      observer.observe(section);
+    });
+  }, []);
+
   return (
     <main className="app__main">
       <Welcome />
--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -1,14 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CgClose } from "react-icons/cg";
+import { motion } from "framer-motion";
+
+import "./Navbar.css";
 
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  function navigateToSection(event, id) {
+    event && event.preventDefault();
+    const elementToView = document.getElementById(id);
+    elementToView.scrollIntoView();
+    setTimeout(() => {
+      setToggleMenu(false);
+    }, 0.2);
+  }
   return (
     <nav className="app__navbar">
-      <a href="#about_me">About Me</a>
-      <a href="#experience">Experience</a>
-      <a href="#skills">Skills</a>
-      <a href="#contact">Contact</a>
+      <button
+        className="app__navbar__button"
+        onClick={(event) => {
+          navigateToSection(event, "about_me");
+        }}
+      >
+        About Me
+      </button>
+      <button
+        className="app__navbar__button"
+        onClick={(event) => {
+          navigateToSection(event, "experience");
+        }}
+      >
+        Experience
+      </button>
+      <button
+        className="app__navbar__button"
+        onClick={(event) => {
+          navigateToSection(event, "skills");
+        }}
+      >
+        Skills
+      </button>
+      <button
+        className="app__navbar__button"
+        onClick={(event) => {
+          navigateToSection(event, "contact");
+        }}
+      >
+        Contact
+      </button>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          fontSize={27}
+          className="overlay__open"
+          onClick={() => setToggleMenu(true)}
+        />
+
+        {toggleMenu && (
+          <motion.div
+            initial={{ scale: 0.5 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.3 }}
+            className="app__navbar-smallscreen_overlay"
+          >
+            <CgClose
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <button
+              onClick={(event) => {
+                navigateToSection(event, "about_me");
+              }}
+            >
+              About Me
+            </button>
+            <button
+              onClick={(event) => {
+                navigateToSection(event, "experience");
+              }}
+            >
+              Experience
+            </button>
+            <button
+              onClick={(event) => {
+                navigateToSection(event, "skills");
+              }}
+            >
+              Skills
+            </button>
+            <button
+              onClick={(event) => {
+                navigateToSection(event, "contact");
+              }}
+            >
+              Contact
+            </button>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
